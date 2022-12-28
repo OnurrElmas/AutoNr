d266c82b3eebc14a1211d4013fde78f9a08428ea
Onur Elmas <57056680+OnurrElmas@users.noreply.github.com>
2022-12-28 14:23:36 +0100

    Update excel.py
    almost done

--- excel.py
+++ excel.py
@@ -1,16 +1,32 @@
 import tkinter as tk    
 from tkinter import ttk
 from tkinter import *
+import pandas as pd
+import openpyxl
 
-lues=["Choice 1","Second choice","Something","Else"]
-boxes=["Tesxt1","Tesxt2","Tesxt3","Tesxt4"]
+
+def connect_with_excel():
+        
+    path = "C:\\Users\\ONURELMA\\Desktop\PDFs\\pandas.xlsm"
+    wb_obj = openpyxl.load_workbook(path)
+    sheet_obj = wb_obj.active
+    cellstrings = []
+    cellnumbers = []
+    for i in range (1,100):
+        cell_obj = sheet_obj.cell(row = 1, column = i).value
+        cellstring = str(cell_obj)
+        if cellstring.startswith('SW_'):
+            cellstrings.append(cellstring)
+    return cellstrings
+
+
 class ComboboxSelectionWindow():
     def __init__(self, master):
         self.master=master
         self.entry_contents=None
         self.labelTop = tk.Label(master,text = "Select one of the SW Version")
         self.labelTop.place(x = 30, y = 10, width=140, height=10)
-        self.comboBox_example = ttk.Combobox(master,values=lues)
+        self.comboBox_example = ttk.Combobox(master,values=cellstrings)
         self.comboBox_example.current(0)
         self.comboBox_example.place(x = 20, y = 30, width=140, height=25)
 
@@ -31,7 +47,6 @@ def ComboboxSelection():
     app.mainloop()
 
     #print("Selected interface: ", Selection.comboBox_example_contents)
-
     return Selection.comboBox_example_contents
 
 
@@ -39,9 +54,16 @@ class CheckBoxSelectionWindow():
     def __init__(self, master):
         self.master=master
         self.entry_contents=None
-        self.chvar = tk.IntVar()
-        self.chvar.set(0)
-        self.checkbox_example = ttk.Checkbutton(master,text=box,variable=self.chvar).grid(row,column)
+        self.chvars = []
+
+        self.chvar_1 = tk.IntVar()
+        self.chvar_1.set(0)
+        self.checkbox_example = ttk.Checkbutton(master,text="Integration",variable=self.chvar_1).grid(row=0,column=0)
+        
+
+        self.chvar_2 = tk.IntVar()
+        self.chvar_2.set(0)
+        self.checkbox_example = ttk.Checkbutton(master,text="Fusi Prio1",variable=self.chvar_2).grid(row=10,column=0)
         
 
         self.okButton = tk.Button(master, text='OK',command = self.callback)
@@ -59,14 +81,56 @@ def CheckBoxSelection():
     app.geometry('500x250')
     Selection=CheckBoxSelectionWindow(app)
     app.mainloop()
-    
+    chvars = []
+    chvars.append(Selection.chvar_1.get())
+    chvars.append(Selection.chvar_2.get())
     #print("Selected interface: ", Selection.comboBox_example_contents)
 
-    return Selection.chvar.get()
+    return chvars
+
+def find_column(comboselection):
+    path = "C:\\Users\\ONURELMA\\Desktop\\PDFs\\pandas.xlsm"
+    wb_obj = openpyxl.load_workbook(path)
+    sheet_obj = wb_obj.active
+    cellstrings = []
+    for i in range (1,100):
+        cell_obj = sheet_obj.cell(row = 1, column = i).value
+        cellstring = str(cell_obj)
+        if cellstring == comboselection:
+            h=i
+    return h
+    
+def main_function(columnnumber):
+    path = "C:\\Users\\ONURELMA\\Desktop\\PDFs\\pandas.xlsm"
+    wb_obj = openpyxl.load_workbook(path)
+    sheet_obj = wb_obj.active
+    cellstrings = []
+    for i in range (3,700):
+        cell_obj = sheet_obj.cell(row = i, column = columnnumber).value
+        cellstring = str(cell_obj)
+        if (cell_obj is None or cellstring == 'R' or cellstring == 'O') and sheet_obj.cell(row = i, column = 11).value == '1M' and sheet_obj.cell(row = i, column = 16).value is None:
+            cellstrings.append(sheet_obj.cell(row = i, column = 8).value)
+    return cellstrings
+    
+
+def write_in_txt(tc_numbers):
+    with open("C:\\Users\\ONURELMA\\Desktop\\PDFs\\NightRun.txt","w") as file1:
+        for tc_number in tc_numbers:
+            file1.write("%s\n" %tc_number)
+    file1.close
+    
 
 
+
+
+cellstrings = connect_with_excel()
 
 comboselection = ComboboxSelection()
-seconselection = CheckBoxSelection()
+checkboxselection = CheckBoxSelection()
 print(comboselection)
-print(seconselection)
+print(checkboxselection)
+
+columnnumber = find_column(comboselection)
+print(columnnumber)
+tc_numbers =main_function(columnnumber)
+write_in_txt(tc_numbers)
